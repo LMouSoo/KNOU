e0d9b5da755464b41c8de16d712ab8c9e0ee8f64
김현수 <soo9211@gmail.com>
2020-11-24 09:39:49 +0000

    출석대체과제 마무리

--- C++Programming/polygon.h
+++ C++Programming/polygon.h
@@ -1,18 +1,19 @@
-#pragma once
+#pragma once    // polygon.h가 중복 include 되지 않도록 체크 (C++11 이상)
 struct C2D {
     double x, y; // x축 좌표와 y축 좌표
 };
 class polygon {
 private:
-    int number_of_vertices;
-    C2D *vertex_coordinates;
+    int number_of_vertices;                                             // 다각형 꼭짓점 갯수
+    C2D *vertex_coordinates;                                            // 다각형 꼭짓점 좌표 저장 포인터
 public:
-    polygon(const int numberofvertices, const C2D *vertexcoordinates);
-    polygon(const polygon& polygonobject);
-    polygon(polygon&& polygonobject);
-    ~polygon();
-    bool simple_or_complex() const;
-    double area_of_polygon() const;
-    void print_polygon_info() const;
-    polygon move_polygon(const double x, const double y) ;
+    polygon(const int numberofvertices, const C2D *vertexcoordinates);  // 생성자
+    polygon(const polygon& polygonobject);                              // 복사 생성자
+    polygon(polygon&& polygonobject);                                   // 이동 생성자
+    ~polygon();                                                         // 소멸자
+    //bool simple_or_complex() const;    신발끈공식은 simple polygon에서만 사용가능하기때문에 판정함수를 만드려 했으나 보류
+    double area_of_polygon() const;                                     // 다각형의 넓이를 반환
+    void print_polygon_info() const;                                    // 다각형의 정보를 출력
+    void vertex_coordinates_memory_address() const;                     // 다각형 꼭짓점 좌표 저장 포인터 출력
+    polygon move_polygon(const double x, const double y);               // 위치를 옮긴 다각형을 반환
 };
--- C++Programming/testcode.cpp
+++ C++Programming/testcode.cpp
@@ -7,7 +7,7 @@ int main ()
     int inputnumberofvertices;
     double x, y;
     cout << "꼭짓점의 갯수를 정수로 입력하세요" <<endl;
-    cin >> inputnumberofvertices;
+    cin >> inputnumberofvertices;                                          //사용자로부터 다각형의 정보를 입력받음
     if (inputnumberofvertices<3){
         cout << "다각형은 세 개 이상의 꼭짓점 좌표로 정의됩니다." << endl;
         return 0;
@@ -17,13 +17,32 @@ int main ()
         cout << i+1 << "번째 꼭짓점의 x좌표 y좌표를 입력하세요." << endl;
         cin >> x >> y;
         inputvertexcoordinates[i] = {x, y};
+        //각각의 꼭짓점의 위치가 동일한지 아닌지 판정이 필요할 수 있으나 보류
     }
+
+    cout << "==========poly1 생성==========" << endl;
     polygon poly1(inputnumberofvertices, inputvertexcoordinates);
+    cout << "poly1의 넓이는 " << poly1.area_of_polygon() << endl;
     poly1.print_polygon_info();
-    cout << poly1.area_of_polygon() << endl;
+    cout << "이 다각형의 꼭짓점의 좌표가 저장된 메모리 위치는" <<endl;
+    poly1.vertex_coordinates_memory_address();
+
+    cout << "==========poly1을 복사해 poly2 생성==========" << endl;
     polygon poly2(poly1);
+    cout << "이 다각형의 넓이는 " << poly2.area_of_polygon() << endl;
     poly2.print_polygon_info();
-    polygon poly3(move(poly2));
+    cout << "이 다각형의 꼭짓점의 좌표가 저장된 메모리 위치는" <<endl;
+    poly2.vertex_coordinates_memory_address();
+
+    cout << "==========poly1을 이동해 poly3 생성==========" << endl;
+    polygon poly3(move(poly1));                                     //move()를 이용해 이동 생성자를 호출
+    cout << "이 다각형의 넓이는 " << poly3.area_of_polygon() << endl;
     poly3.print_polygon_info();
-    poly2.print_polygon_info();
+    cout << "이 다각형의 꼭짓점의 좌표가 저장된 메모리 위치는" <<endl;
+    poly3.vertex_coordinates_memory_address();
+
+    cout << "==========이동된 poly1의 정보는 없어짐==========" << endl;
+    poly1.print_polygon_info();
+    cout << "메모리 위치도 비워짐" << endl;
+    poly1.vertex_coordinates_memory_address();
 }
